5c4dbd716fb7481d2d6226244a35cbde1e7879f3
lohade <abhisheklohade8584@gmail.com>
2021-10-01 19:59:06 +0530

    last name validation

--- src/com/validation/ValidationCalculate.java
+++ src/com/validation/ValidationCalculate.java
@@ -1,14 +1,38 @@
 package com.validation;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ValidationCalculate {
-    String validation="[A-Z]{1}+[a-z]{2,}";
+    Pattern pattern;
+    Matcher matcher;
+    String FirstName_validation="[A-Z]{1}+[a-z]{2,}";
+    String LastName_validation="[A-Z]{1}+[a-z]{2,}";
+    //String email_validation="[a-zA-Z0-9.]+@[a-z.]+[a-z]";
+
+
     public void Firstvalidation(String name){
-        boolean result= Pattern.matches(validation,name);
+        boolean result= Pattern.matches(FirstName_validation,name);
         System.out.println("result of validation:"+result);
         if( result == false){
             System.out.println("Should enter start with capital letter or Minimum length is 3");
         }
     }
+    public void LastNameValidation(String surname){
+        pattern=Pattern.compile(LastName_validation);
+        matcher=pattern.matcher(surname);
+        boolean result= matcher.matches();
+        System.out.println(result);
+        if(result==false){
+            System.out.println("Enter validate Surname");
+        }
+    }
+    /*public void email(String email){
+        boolean result=Pattern.matches(email_validation,email);
+        System.out.println(result);
+        if(result==false){
+            System.out.println("enter validation email");
+        }
+    }*/
+
 }
--- src/com/validation/ValidationMain.java
+++ src/com/validation/ValidationMain.java
@@ -7,8 +7,33 @@ public class ValidationMain {
 	// write your code here
         Scanner scanner=new Scanner(System.in);
         ValidationCalculate validationCalculate=new ValidationCalculate();
-        System.out.println("Enter first name for validation:");
-        String name = scanner.next();
-        validationCalculate.Firstvalidation(name);
+        boolean status=true;
+        do {
+            System.out.println("1:first name validation:\n2:Last name validation:\n3:Email validation:\n");
+            int choice= scanner.nextInt();
+            switch(choice) {
+                case 1:
+                System.out.println("Enter first name for validation:");
+                String name = scanner.next();
+                validationCalculate.Firstvalidation(name);
+                break;
+
+                case 2:
+                System.out.println("Enter last name for validation:");
+                String surname = scanner.next();
+                validationCalculate.LastNameValidation(surname);
+                break;
+                /*
+                case 3:
+                System.out.println("enter email to validation:");
+                String email = scanner.next();
+                validationCalculate.email(email);
+                break;*/
+
+                case 3:
+                    status=false;
+                    break;
+            }
+        }while(status);
     }
 }
